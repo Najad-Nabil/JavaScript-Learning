195bcdde37f14d1843edf022ad75c90b9d779f0f
Najad-Nabil <najadnabil2612@gmail.com>
2026-06-12 14:47:59 +0000

    Step One Of Learning

---
+++ stepOne.js
@@ -0,0 +1,17 @@
+function addValue(a, b){
+    return a+b;
+}
+
+function subValue(a, b){
+    return a - b;
+}
+
+function mulValue(a, b){
+    return a * b;
+}
+
+function divValue(a, b){
+    return a / b;
+}
+
+console.log('Sum = '+addValue(10, 5)+'\nSubstrantion = '+subValue(10, 5)+'\nProduct = '+mulValue(10, 5)+'\nDivident = '+divValue(10, 5));
