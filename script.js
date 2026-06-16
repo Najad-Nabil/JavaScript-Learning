aca69ba243adb7a9edce4491655dbc88c7e018be
Najad-Nabil <najadnabil2612@gmail.com>
2026-06-16 17:45:27 +0000

    Basic frontend

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 let student = [];
 
+let inputBox = document.getElementById("studentName");
+inputBox.addEventListener("keydown" , function(event){
+    if(event.key === "Enter"){
+        addName();
+    }
+})
+
 function addName(){
     let name = document.getElementById("studentName").value;
     student.push(name);
@@ -8,7 +15,15 @@ function addName(){
 }
 
 function showAllStudents(){
+
+    let outputDiv = document.getElementById("output");
+    outputDiv.innerHTML = "";
+    let numberOfStudents = document.createElement("p")
+    numberOfStudents.textContent = ("Total number of students = "+student.length)
+    outputDiv.appendChild(numberOfStudents)
     for(let i=0;i<student.length;i++){
-        document.getElementById("output").textContent = (i+". "+student[i]);
+        let p = document.createElement("p");
+        p.textContent = ((i+1)+". "+student[i]);
+        outputDiv.appendChild(p);
     }
 }
